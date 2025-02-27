92c81bbe15cb65ab78b97ebee06c3a76b91bae28
Sergio Ciufo <carminesergio.ciufo@gmail.com>
2025-02-27 17:30:27 +0100

    minor fix

--- src/test/java/com/example/autenticationservice/application/mapper/AutenticationMappersImplTest.java
+++ src/test/java/com/example/autenticationservice/application/mapper/AutenticationMappersImplTest.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class AutenticationMappersImplTest {
